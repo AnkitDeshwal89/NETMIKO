41ebda0ed3f628ee4b00e71a38c128a61ad90563
ankit deshwal <ankit deshwal>
2021-11-20 18:33:29 +0530

    script updated

--- test_dict.py
+++ test_dict.py
@@ -7,9 +7,13 @@ device1= {
         "password":getpass(),
         "secret":getpass(),
         "device_type":"cisco_ios",
-        #"session_log":"my_session.txt"
+        "session_log":"my_session.txt"
         }
 
 net_connect = ConnectHandler(**device1)
 net_connect.enable()
-print(net_connect.find_prompt())
+command = "delete unix:/R1-Config-3"
+output = net_connect.send_command(command,expect_string=r"\?",strip_prompt=False,strip_command=False)
+output += net_connect.send_command("",expect_string=r'confirm',strip_prompt=False,strip_command=False)
+output += net_connect.send_command("",expect_string=r'#',strip_prompt=False,strip_command=False)
+print(output)
